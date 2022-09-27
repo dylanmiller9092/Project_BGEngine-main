d15175c27c078a87a88e613d4ae2f39062e6c238
dylanmiller9092 <9092@ait.nsw.edu.au>
2022-09-27 12:15:12 +1000

    Initial commit

---
+++ Texture.cpp
@@ -0,0 +1,83 @@
+#include "Texture.h"
+#include "SDL.h"
+#include "SDL_image.h"
+#include <iostream>
+
+using namespace std;
+
+Texture::Texture()
+{
+	//set default texture to null pointer
+	SdlTexture = nullptr;
+	//set the width and height to 0
+	width = 0;
+	height = 0;
+
+}
+
+Texture::~Texture()
+{
+	ResetTexture();
+}
+
+bool Texture::LoadImageFromFile(const char* path, SDL_Renderer* renderer)
+{
+	if (SdlTexture == nullptr) {
+		//Load the image to the surface
+		SDL_Surface* LoadSurface = IMG_Load(path);
+
+		if (LoadSurface != nullptr) {
+			cout << "Load Texture - Success!" << endl;
+
+			//create the texture from the surface
+			SdlTexture = SDL_CreateTextureFromSurface(renderer, LoadSurface);
+
+			if (SdlTexture != nullptr) {
+				cout << "Create texture from surface - success!" << endl;
+
+				//get the dimensions of the image
+				width = LoadSurface->w;
+				height = LoadSurface->h;
+			}
+			else {
+				cout << "Create texture from surface - failed..." << endl;
+			}
+
+			SDL_FreeSurface(LoadSurface);
+		}
+		else {
+			cout << "Load Texture - failed...: " << SDL_GetError() << endl;
+		}
+	}
+	else {
+		cout << "Remove the existing texture before loading" << endl;
+		return false;
+	}
+	return SdlTexture != nullptr;
+}
+
+void Texture::Draw(SDL_Renderer* renderer, int x, int y, SDL_Rect* SourceRect)
+{
+	//set the rendering space and render dimensions of the texture
+	SDL_Rect DestinationRect = { x, y, width, height };
+
+	//clip the image if we have a source rect
+	if (SourceRect != nullptr) {
+		DestinationRect.w = SourceRect->w;
+		DestinationRect.h = SourceRect->h;
+	}
+
+	//render to the screen
+	SDL_RenderCopy(renderer, SdlTexture, SourceRect, &DestinationRect);
+}
+
+void Texture::ResetTexture()
+{
+	//deallocate Texture
+	if (SdlTexture != nullptr) {
+		SDL_DestroyTexture(SdlTexture);
+		SdlTexture = nullptr;
+		width = 0;
+		height = 0;
+	}
+}
---
+++ Texture.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <SDL.h>
+
+class Texture {
+public:
+	Texture();
+	~Texture();
+
+private:
+	// hold reference to the texture file
+	SDL_Texture* SdlTexture;
+
+	//set dimensions for the file
+	int width;
+	int height;
+
+public:
+	//load the image from path
+	//add the texture to the renderer
+	bool LoadImageFromFile(const char* path, SDL_Renderer* renderer);
+
+	//render the image to the screen when called
+	//param 1 - to copy the texture of the render
+	//param 2 & 3 - x and y position of the texture on the screen
+	//param 4 - the area we want to copy from the texture onto the window
+	void Draw(SDL_Renderer* renderer, int x = 0, int y = 0, SDL_Rect* SourceRect = NULL);
+
+	//return the dimensions of the image
+	int GetImageWidth() { return width; }
+	int GetImageHeight() { return height; }
+
+	//remove the texture from memory
+	void ResetTexture();
+};
